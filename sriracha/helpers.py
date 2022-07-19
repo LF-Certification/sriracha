4daf4f0987da6d9b7bdf49daeeb872dfaa07c5d5
Paul Myjavec <pauly@myjavec.com>
2022-07-19 14:34:59 +0900

    add new things

---
+++ sriracha/helpers.py
@@ -0,0 +1,4 @@
+# Givena string or an int, and compare a with b, as strings.
+def compare(a, b):
+    """Regardless is a string is an int or string, compare the values"""
+    return str(a) == str(b)
---
+++ sriracha/scoring.py
@@ -0,0 +1,39 @@
+import json
+
+# This decorator should be used to decorate all checks.
+def check(func):
+    def inner(*args, **kwargs):
+        returned_value = func(*args, **kwargs)
+        function_name = func.__name__
+        globals()["CHECK_RESULTS"][function_name] = returned_value
+        return returned_value
+
+    return inner
+
+
+def get_score_report():
+    check_results = globals()["CHECK_RESULTS"]
+    checks_executed: int = len(check_results.keys())
+    checks_passed: int = 0
+    checks_successful = []
+    checks_failed = []
+
+    for name, result in check_results.items():
+        if result:
+            checks_passed += 1
+            checks_successful.append(name)
+            continue
+
+        checks_failed.append(name)
+
+    return {
+        "checks_executed": checks_executed,
+        "checks_passed": checks_passed,
+        "checks_successful": checks_successful,
+        "checks_failed": checks_failed,
+        "output_version": 1,
+    }
+
+
+def report_score():
+    print(json.dumps(get_score_report()))

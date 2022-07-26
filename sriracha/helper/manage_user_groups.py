473bbf7322383d302688935bec7bb77b3f0ae881
ramreddyamanaganti <57209182+ramreddyamanaganti@users.noreply.github.com>
2022-07-26 08:56:09 +0000

    Addressed comments

--- sriracha/helper/manage_user_groups.py
+++ sriracha/helper/manage_user_groups.py
@@ -5,23 +5,23 @@ import spwd
 import subprocess
 import sys
 
-def user_exist(username):
+def user_exists(username):
     """Checks User existence"""
     try:
         pwd.getpwnam(username)
         return True
-    except:  # pylint: disable=bare-except
+    except KeyError as e:
         log.info("User someusr does not exist.")
 
     return False
 
 
-def group_exist(groupname):
+def group_exists(groupname):
     """Checks group existence"""
     try:
         grp.getgrnam(groupname)
         return True
-    except:  # pylint: disable=bare-except
+    except KeyError as e:
         log.info("Group %s does not exist.", groupname)
 
     return False
@@ -32,7 +32,7 @@ def check_passwd(username, expected_passwd):
     try:
         password = spwd.getspnam(username).sp_pwdp
         return password == crypt.crypt(expected_passwd, password)
-    except:  # pylint: disable=bare-except
+    except KeyError as e:
         log.info("Failing to get passsword for the user %s", username)
 
     return False
@@ -40,45 +40,49 @@ def check_passwd(username, expected_passwd):
 
 def check_gid(groupname, expected_gid):
     """Checks groupid for groupname"""
+    actual_gid = None
     try:
         actual_gid = grp.getgrnam(groupname).gr_gid
-
-        if actual_gid == int(expected_gid):
-            return True
-    except:  # pylint: disable=bare-except
+    except KeyError as e:
         log.info("Failed to get group id for %s", groupname)
+
+    if actual_gid == int(expected_gid):
+            return True
 
     return False
 
 
-def user_exists_in_group(groupname, username):
+def user_in_group(groupname, username):
     """Checks user existence in the group"""
+    grp_mems = None
     try:
         grp_mems = grp.getgrnam(groupname).gr_mem
+    except KeyError as e:
+        log.info("Failed to get group members of the group %s", groupname)
 
-        if username in grp_mems:
-            return True
-    except:  # pylint: disable=bare-except
-        log.info("Faileed to get group members of the group %s", groupname)
-
+    if username in grp_mems:
+        return True
     return False
 
 
 def check_user_primary_grp(groupname, username):
     """Checks primary group of the user"""
+    p_gid = None
+    actual_grp_name = None
     try:
         p_gid = pwd.getpwnam(username).pw_gid
         actual_grp_name = grp.getgrgid(p_gid).gr_name
 
-        if actual_grp_name == groupname:
-            return True
-    except:  # pylint: disable=bare-except
+    except KeyError as e:
         log.info("Failed to get user group of %s", groupname)
+
+    if actual_grp_name == groupname:
+        return True
 
     return False
 
 
-def check_non_interactive_shell(username):
+def user_shell_is_not_interactive(username):
     """Checks for non interactive shell of a user"""
     try:
         expected_shells = ["/bin/false", "/sbin/nologin"]
@@ -96,3 +100,6 @@ def check_non_interactive_shell(username):
         log.info("Failed to get the user shell for %s", username)
 
     return False
+
+
+
